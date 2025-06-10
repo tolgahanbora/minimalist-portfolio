18818c2655e9c400c9e2a012fbe5ae374f8158bc
Tolgahan Bora <tolgahan.bora@tasarlab.com>
2025-06-10 18:06:25 +0300

    Add Vercel Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,9 @@ import 'nextra-theme-blog/style.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/main.css'
+import { inject } from '@vercel/analytics'
+
+inject()
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
